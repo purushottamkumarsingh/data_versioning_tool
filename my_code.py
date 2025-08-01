99a624b1666c227070e47eac3434a96fa2faed14
purushottamkumarsingh <ravindrasingh813108@gmail.com>
2025-08-01 14:16:20 +0530

    second version correctd one

--- my_code.py
+++ my_code.py
@@ -9,6 +9,9 @@ data ={ 'Name':['alice','bob','charlie'],
 df = pd.DataFrame(data)
 new_row_loc ={'Name':'purushottam','age':23,'city':'bihar'}
 df.loc[len(df.index)] =new_row_loc
+new_row_loc1 ={'Name':'purushottam_singh_rajput','age':21,'city':'bhagalpur'}
+df.loc[len(df.index)] =new_row_loc1
+
 data_dir ='data'
 os.makedirs(data_dir,exist_ok=True)
 file_path = os.path.join(data_dir ,'sample_data.csv')
